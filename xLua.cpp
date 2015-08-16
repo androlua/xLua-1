2bf9e4c58943b473c8fa9ff99343b63d3bf48481
bysdxt <bysdxt@qq.com>
2015-08-16 15:16:28 +0800

    vc项目加入lua53代码; 调整编译选项; 调整main函数;

--- xLua.cpp
+++ xLua.cpp
@@ -1,4 +1,13 @@
-int main() {
-    return 0;
+extern "C" {
+    int luac_main(int argc, char* argv[]);
+    int lua_main(int argc, char **argv);
 }
 
+int main(int argc, char* argv[]) {
+    auto argv1 = argv[1];
+    if (argc > 1 && argv1[0] == '-' && argv1[1] == 'c' && argv1[2] == '\0') {
+        argv[1] = *argv;
+        return luac_main(--argc, argv + 1);
+    }
+    return lua_main(argc, argv);
+}
